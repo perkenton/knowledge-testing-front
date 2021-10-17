45b885fa0e4f06cd82be935bd6fcf9d863b72fb8
Alexander Vasiliev <selado@ya.ru>
2021-10-17 22:22:22 +0500

    Создан компонент для регистрации пользователей

---
+++ src/app/user-registration/user-registration.component.ts
@@ -0,0 +1,13 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-user-registration',
+  templateUrl: './user-registration.component.html',
+  styleUrls: ['./user-registration.component.scss'],
+  // interpolation: [ '{{', '}}' ],
+})
+export class UserRegistrationComponent implements OnInit {
+
+  ngOnInit() {}
+
+}
