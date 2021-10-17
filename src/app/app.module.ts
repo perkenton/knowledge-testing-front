8421e8c156b71a1319e109558d1ab641d6d8d8f5
Alexander Vasiliev <selado@ya.ru>
2021-10-17 22:23:56 +0500

    Импортированы и прописаны компоненты и модули

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,27 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+import { registerLocaleData } from '@angular/common';
+import localeRu from '@angular/common/locales/ru';
 
 import { AppComponent } from './app.component';
+import { UserRegistrationComponent } from './user-registration/user-registration.component';
+import { TestFormComponent } from './test-form/test-form.component';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+
+registerLocaleData(localeRu, 'ru');
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UserRegistrationComponent,
+    TestFormComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    NoopAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
