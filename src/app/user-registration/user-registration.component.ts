71eb7795c0c005ad8cdfb5ffe04af07573d9d2d4
Alexander Vasiliev <selado@ya.ru>
2021-10-18 00:29:36 +0500

    Создана и настроена форма регистрации пользователя

--- src/app/user-registration/user-registration.component.ts
+++ src/app/user-registration/user-registration.component.ts
@@ -1,13 +1,29 @@
 import { Component, OnInit } from '@angular/core';
 
+// TODO: тип импортировать с бэка
+export type User = {
+  surname: string;
+  name: string;
+  patronymic: string;
+}
+
 @Component({
   selector: 'app-user-registration',
   templateUrl: './user-registration.component.html',
   styleUrls: ['./user-registration.component.scss'],
   // interpolation: [ '{{', '}}' ],
 })
-export class UserRegistrationComponent implements OnInit {
+export class UserRegistrationComponent {
+  // TODO: сделать форму через FormBuilder
 
-  ngOnInit() {}
+  user: User = {
+    surname: '',
+    name: '',
+    patronymic: '',
+  };
+
+  formSubmit() {
+    console.log('name', this.user);
+  }
 
 }
